56315746aee8312d9242a10c65c33e71e4552039
luiznatoo <gabriel.naato@gmail.com>
2023-04-15 02:05:24 -0300

    adicionando restante do layout e animações

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert("Importante: Este site está em desenvolvimento e, portanto, as informações apresentadas não são reais. Agradecemos sua compreensão.")
+ScrollReveal({ reset: true});
+
+
+ScrollReveal().reveal('.area-1', { delay: 300 });
+ScrollReveal().reveal('.area-2', { delay: 400 });
+ScrollReveal().reveal('.area-3', { delay: 500 });
+ScrollReveal().reveal('.area-4', { delay: 600 });
+ScrollReveal().reveal('.area-5', { delay: 700 });
