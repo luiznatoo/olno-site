0915cf2873ca1b5aa4961a6bf176ca6271160faf
Luiz Gabriel <ti@belemriotransportes.com>
2024-03-05 16:56:58 -0300

    primeira

--- script.js
+++ script.js
@@ -6,5 +6,3 @@ ScrollReveal().reveal('.area-2', { delay: 400 });
 ScrollReveal().reveal('.area-3', { delay: 500 });
 ScrollReveal().reveal('.area-4', { delay: 600 });
 ScrollReveal().reveal('.area-5', { delay: 700 });
-
-alert("Site em desenvolvimento")
